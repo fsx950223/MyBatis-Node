242bd78c371127f335401a3d872844f66530e937
fsx950223 <fsx950223@outlook.com>
2018-04-23 11:53:25 +0800

    refactor(typescript): use typescript refacts code

---
+++ src/Context.ts
@@ -0,0 +1,77 @@
+import * as uuid from "uuid";
+class Context {
+    public callbacks;
+    public id;
+    public connection;
+    public loading;
+    constructor() {
+        this.callbacks = [];
+        this.id = uuid.v4();
+    }
+    public uploaded(connection) {
+        this.connection = connection;
+        this.loading = false;
+        for (const callback of this.callbacks) {
+            callback(this.connection);
+        }
+    }
+    public getConnected(callback, pool) {
+        if (this.connection) {
+            return callback(this.connection);
+        }
+        this.callbacks.push(callback);
+        if (this.loading === true) {
+            return;
+        }
+        this.loading = true;
+        pool.getConnection((err, connection) => {
+            if (err) {
+                throw err;
+            }
+            this.uploaded(connection);
+        });
+    }
+    public initiationTransaction(callback, pool) {
+        const withchange = (callback) => {
+            this.connection.beginTransaction(() => {
+                return callback(this.connection, (success, error) => {
+                    this.commit(success);
+                });
+            });
+        };
+        if (this.connection) {
+            return withchange(callback);
+        }
+        this.getConnected((connection) => {
+            withchange(callback);
+        }, pool);
+    }
+    public release() {
+        if (this.connection) {
+            this.connection.release();
+        }
+    }
+    public commit(callback) {
+        if (!this.connection) {
+            return;
+        }
+        this.connection.commit( (result, err) => {
+            if (err) {
+                this.connection.rollback(() => {
+                    if (callback) {
+                        callback(false);
+                    }
+                });
+            } else if (callback) {
+                callback(true);
+                   }
+        });
+    }
+    public rollback() {
+        if (!this.connection) {
+            return;
+        }
+        this.connection.rollback();
+    }
+}
+export default Context;
